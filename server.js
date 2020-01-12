1bdff2ecc566d5f4a8d5289ac4023a99d28ac6db
Valeri Tonchev <tonchev2712@gmail.com>
2020-01-12 14:44:00 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(request, response){
+  response.send("Hello");
+})
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000")
+});
